d365a77edc2fde4a2d5e7faaa1c84f204784ce54
Aidan <torrinfail@gmail.com>
2018-03-04 21:33:14 +0000

    more stuff

--- BinarySearchDeluxe.java
+++ BinarySearchDeluxe.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Comparator;
+import edu.princeton.cs.algs4.BinarySearch;
 public class BinarySearchDeluxe
 {
 	public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator)
@@ -15,7 +16,7 @@ public class BinarySearchDeluxe
 		if(a == null || key == null || comparator == null)
 			throw new NullPointerException("One of the passed in arguments is null.");
 		comparator = new ReverseComparator(comparator);
-		
+
 		return Arrays.binarySearch(a, key, comparator);	
 	}
 
@@ -31,5 +32,23 @@ public class BinarySearchDeluxe
 		{
 			return -(comparator.compare(t1,t2));
 		}
+	}
+
+	private static <Key> int indexOf(Key[] a, Key key, Comparator<Key> comparator, boolean getLast) 
+	{
+		int lo = 0;
+		int hi = a.length - 1;
+		int mid;
+		int returnVal = -1;
+		while (lo <= hi) {
+			// Key is in a[lo..hi] or not present.
+			mid = lo + (hi - lo) / 2;
+			if      (comparator.compare(key,a[mid]) < 0)
+			       	hi = mid - 1;
+			else if (comparator.compare(key,a[mid]) > 0)
+			       	lo = mid + 1;
+			else return mid;
+		}
+		return returnVal;
 	}
 }
